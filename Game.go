9ac4deaf8f01d4c3a60b8adabb7c5fb316e77759
viraat0700 <viraat.shrivastava0700@gmail.com>
2024-05-07 18:39:15 +0530

    Commit to Tic-Tac-Toe Branch

--- Game.go
+++ Game.go
@@ -1,108 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
+	"fmt"
 )
 
-var board [3][3]string
-var currentPlayer string
-
 func main() {
-    initBoard()
-    currentPlayer = "X"
-    clearScreen()
-    printBoard()
+	board := [3][3]string{{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}} // hamne ek board Name ka variable initialize kiya hai and vo ek array jisme hamne haar ek cell ko "-" se fill kiya hai
+	currentPlayer := "X" // fir maine default value ya default player ko "X" maan liya hai 
+	fmt.Println("Welcome to Tic Tac Toe!")
 
-    for {
-        fmt.Printf("Player %s's turn\n", currentPlayer)
-        fmt.Print("Enter row and column (e.g., 1 2): ")
-        var row, col int
-        fmt.Scanln(&row, &col)
+	for { // for ka loop tab end hga jab koi bhi player jeet jaye ya tuo draw ho jaye or ya tuo player invalid chezz inter kare
+		fmt.Printf("Player %s's turn: ", currentPlayer) // ye line print karegi ki kiski turn hai "x"/"O"
+		var row, col int // Maine yaha per 2 variable ko declare kiya hai 
+		fmt.Scanln(&row, &col) //Scanln se ham user se input lenge row/col ka
 
-        if isValidMove(row-1, col-1) {
-            board[row-1][col-1] = currentPlayer
-            clearScreen()
-            printBoard()
+		if row < 1 || row > 3 || col < 1 || col > 3 || board[row-1][col-1] != "_" { // Yaha per ham check kar rhe hai ki inputs valid hai ki nahi 
+			fmt.Println("Invalid move. Please try again.")
+			continue
+		}
 
-            if checkWin() {
-                fmt.Printf("Player %s wins!\n", currentPlayer)
-                break
-            }
+		board[row-1][col-1] = currentPlayer // yaha per ham tictactoe board per jate hai and ham Player ka symbol input karte hai "X/O"
+		fmt.Println("  1 2 3") // Ise line ke through ham Har ek column ko mark kar rhe hai from 1,2,3
+		for i := 0; i < 3; i++ { // ise loop ko use kar rhe hai because hame rows ko bhi mark karna hai
+			fmt.Printf("%d ", i+1) // ye line print kar rhi / mark kar rhi hai har ek row ko
+			for j := 0; j < 3; j++ { // we are iterating over each column because abhi hamne symbol "x"/"O" ko print karwana hai board per 
+				fmt.Printf("%s ", board[i][j])//this is how we print the symbol
+			}
+			fmt.Println()
+		}
 
-            if checkDraw() {
-                fmt.Println("It's a draw!")
-                break
-            }
 
-            switchPlayer()
-        } else {
-            fmt.Println("Invalid move. Please try again.")
-        }
-    }
-}
+		//3 conditions jab players jeet ta hai row mein same symbol align hue / column mein / diagnolly 
+		if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+			(board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+			fmt.Printf("Player %s wins!\n", currentPlayer)
+			break
+		}//diagonally checking
 
-func initBoard() {
-    for i := 0; i < 3; i++ {
-        for j := 0; j < 3; j++ {
-            board[i][j] = " "
-        }
-    }
-}
+		for i := 0; i < 3; i++ {
+			if board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer {
+				fmt.Printf("Player %s wins!\n", currentPlayer)
+				return
+			}
+			if board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer {
+				fmt.Printf("Player %s wins!\n", currentPlayer)
+				return
+			}
+		}// rows and column check kar rhe hai 
 
-func printBoard() {
-    for i := 0; i < 3; i++ {
-        fmt.Printf(" %s | %s | %s\n", board[i][0], board[i][1], board[i][2])
-        if i < 2 {
-            fmt.Println("---+---+---")
-        }
-    }
-}
+		draw := true //initially draw ko true rakha hai because agar last mein koi space khali bachi tuo draw ni hga tan mujhe draw ki value ko false karna hoga haar ek cell check hora ki space " " tuo ni hai 
+		for i := 0; i < 3; i++ {
+			for j := 0; j < 3; j++ {
+				if board[i][j] == "_" {
+					draw = false
+					break
+				}
+			}
+		}
+		if draw { // Agar draw ki value check karne ke badh true hue tuo ye chalega varna nahi 
+			fmt.Println("It's a draw!")
+			break
+		}
 
-func isValidMove(row, col int) bool {
-    return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == " "
-}
-
-func checkWin() bool {
-    for i := 0; i < 3; i++ {
-        if board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer {
-            return true
-        }
-        if board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer {
-            return true
-        }
-    }
-    if board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer {
-        return true
-    }
-    if board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer {
-        return true
-    }
-    return false
-}
-
-func checkDraw() bool {
-    for i := 0; i < 3; i++ {
-        for j := 0; j < 3; j++ {
-            if board[i][j] == " " {
-                return false
-            }
-        }
-    }
-    return true
-}
-
-func switchPlayer() {
-    if currentPlayer == "X" {
-        currentPlayer = "O"
-    } else {
-        currentPlayer = "X"
-    }
-}
-
-func clearScreen() {
-    cmd := exec.Command("clear") // for Linux/OS X
-    cmd.Stdout = os.Stdout
-    cmd.Run()
+		if currentPlayer == "X" { // Yaha per Players ko switch kar rha hu agar "X" ki chance thi tuo ab "O" ki chance hogi
+			currentPlayer = "O"
+		} else {
+			currentPlayer = "X"
+		}
+	}
 }
